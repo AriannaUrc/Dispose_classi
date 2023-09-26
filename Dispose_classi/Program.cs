d9c4ea1340cf6e620efb57df60797421eb9f42c2
AriannaUrc <urciuoli.arianna.studente@itispaleocapa.it>
2023-09-26 13:53:53 +0200

    ereditarietà

--- Dispose_classi/Program.cs
+++ Dispose_classi/Program.cs
@@ -21,30 +21,34 @@ namespace Dispose_classi
 
             Console.WriteLine("\n\nvalore stringa: " + esempio.Stringa);
             Console.WriteLine("valore numero: " + esempio.Numero);
+            GC.Collect();
 
             Console.WriteLine("\n\n\nChiamiamo la funzione dispose...");
             esempio.Dispose();
 
-            Console.WriteLine("\n\nChiamiamo nuovamente la funzione dispose...\n\n");
+            Console.WriteLine("\n\nChiamiamo nuovamente la funzione dispose...");
             esempio.Dispose();
+
+            
         }
     }
 
 
-    class Esempio
+    class Esempio : IDisposable
     {
         //classe esempio, string e double..? get e set, costruttori distruttore, writeline in dispose
         //attributi
         private string stringa;
-        private double numero;
-        private bool disposed = false;
+        private int numero;
+        private bool disposed;
 
 
         //costruttore con paramentri
-        public Esempio(double valore1, string string1)
+        public Esempio(int valore1, string string1)
         {
             numero = valore1;
             stringa = string1;
+            disposed = false;
         }
 
         //costruttore senza parametri
@@ -52,6 +56,7 @@ namespace Dispose_classi
         {
             numero = 0;
             stringa = "vuoto";
+            disposed = false;
         }
 
 
@@ -64,7 +69,7 @@ namespace Dispose_classi
 
 
         //set e get double
-        public double Numero
+        public int Numero
         {
             get { return numero; }
             set { numero = value; }
@@ -103,6 +108,56 @@ namespace Dispose_classi
         ~Esempio()
         {
             Dispose(false);
+            Console.WriteLine("dispose");
+        }
+    }
+
+    class EsempioEsteso : Esempio
+    {
+        double decimale;
+        private bool disposed;
+
+        public EsempioEsteso()
+        {
+            decimale = 0;
+        }
+        public EsempioEsteso(double val)
+        {
+            decimale = val;
+        }
+
+        public double Decimale
+        {
+            get { return decimale; }
+            set { decimale = value; }
+        }
+
+
+        //aree virtuali
+        protected virtual void DisposeEsteso(bool disposing)
+        {
+            if (disposed)
+            {
+                Console.WriteLine("EX: la risorsa è già stata cancellata...");
+                return;
+            }
+
+
+            if (disposing)
+            {
+                // rilascia le risorse gestite qui (qual'ora ci fossero)
+                Console.WriteLine("EX: sta cancellando...");
+            }
+            // rilascia le risorse non gestite qui (qual'ora ci fossero)
+
+            disposed = true;
+        }
+
+        //Distruttore (generalmente non è necessario/usato in C#)
+        ~EsempioEsteso()
+        {
+            DisposeEsteso(false);
+            Console.WriteLine("EX: dispose");
         }
     }
 }
